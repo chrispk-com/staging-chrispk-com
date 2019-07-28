486e626fe6d6cf0d6a9c90dac1080f4908caef20
chrispk <chrispkennaby@gmail.com>
2019-07-29 00:16:46 +0100

    add css task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,22 @@
 // gulpfile.js
 
-//const gulp = require('gulp');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+
+const srcDir = '_src';
+const cssSrc = srcDir + '/sass/**/*.?(s)css';
+const jsSrc = srcDir + '/js/**/*.js';
+
+gulp.task('css', () => {
+	gulp.src(cssSrc)
+		.pipe(sass())
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('assets'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch(cssSrc, ['sass']);
+});
+
+gulp.task('default', ['css', 'watch']);
